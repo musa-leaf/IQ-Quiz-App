907a1af7e13aa5cd592180a0b4fe00967a998c84
musa-leaf <khosamusa42@gmail.com>
2018-06-29 12:08:48 +0200

    ui

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  timerObject: any;
+  timerValue : number = 0;
+  counter : number = 0;
+  score : number = 0;
+  done : boolean = false;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onStart(){
+    this.timerObject = Observable.interval(1000).subscribe(
+      x => {
+        this.timerValue = x;
+      }
+    );
+    this.proceed();
+  }
+  
+  proceed(){
+    if(this.counter < 5)
+      this.counter++;
+  }
+
+  scoreTracker(arg){
+
+  }
+
+  viewResults(){
+    console.log(this.timerValue + this.score);
   }
 
 }
