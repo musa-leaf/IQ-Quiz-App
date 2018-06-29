891edecbea41e42ab0e8f354898bc7c5634b692f
musa-leaf <khosamusa42@gmail.com>
2018-06-29 14:08:18 +0200

    complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import { collectExternalReferences } from '@angular/compiler';
 
 @Component({
   selector: 'page-home',
@@ -13,7 +14,9 @@ export class HomePage {
   timerValue : number = 0;
   counter : number = 0;
   score : number = 0;
-  done : boolean = false;
+  done : boolean = true;
+  answersUser = ['a1', 'a2','a3','a4','a5'];
+  answers = ['aTrue','bFalse','cFalse','dTrue','eTrue'];
 
   constructor(public navCtrl: NavController) {
 
@@ -25,6 +28,7 @@ export class HomePage {
         this.timerValue = x;
       }
     );
+
     this.proceed();
   }
   
@@ -34,11 +38,26 @@ export class HomePage {
   }
 
   scoreTracker(arg){
-
+    this.answersUser[this.counter-1] = arg;
   }
 
   viewResults(){
-    console.log(this.timerValue + this.score);
+    this.timerObject.unsubscribe();
+    for(let i = 0; i < this.answers.length; i++ ){
+      if(this.answersUser[i] === this.answers[i]){
+        this.score++;
+      }
+    }
+    
+    this.done = true;
+    this.counter = 6;
+  }
+
+  reset(){
+    this.done = false;
+    this.counter = 0;
+    this.score = 0;
+
   }
 
 }
